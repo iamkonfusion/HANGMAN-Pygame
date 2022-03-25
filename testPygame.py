e78729866e6b311f74de7d52ff4cc6f07565a361
iamkonfusion <96213674+iamkonfusion@users.noreply.github.com>
2022-03-25 10:04:58 +0100

    Update testPygame.py

--- testPygame.py
+++ testPygame.py
@@ -4,6 +4,7 @@ from sys import exit
 pygame.init() #starts & initiates pygame (IMPORTANT)
 screen = pygame.display.set_mode((600,400)) #création de la fenêtre + ! ARGUMENT = TUPLE (hauteur,largeur)
 pygame.display.set_caption('HANGMAN')
+clock = pygame.time.Clock() # framerate control
 
 while True:
     for event in pygame.event.get():
@@ -11,4 +12,5 @@ while True:
             pygame.quit()
             exit()
     pygame.display.update()
+    clock.tick(60) # ceiling = 60 FPS
     
