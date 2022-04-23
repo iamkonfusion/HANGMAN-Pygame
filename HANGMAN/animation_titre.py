8011e29a58217085e63669f0c48e1d2e9190e611
iamkonfusion <96213674+iamkonfusion@users.noreply.github.com>
2022-04-23 19:51:52 +0200

    Update animation_titre.py

--- HANGMAN/animation_titre.py
+++ HANGMAN/animation_titre.py
@@ -15,7 +15,6 @@ screen.blit(pygame.transform.smoothscale(background, size),(0,0))
 font = pygame.font.Font("bryndan-write-font/BryndanWrite.ttf", 97)
 # color_values = r, g, b = (94,102,151) # ORIGINAL 'HANGMAN.' COLOR VALUES
 color_values = r, g, b = (248,251,255) # couleur background
-clock = pygame.time.Clock()
 
 while not (r < 94 and g < 102 and b < 151):
     if not r < 94 :
@@ -27,7 +26,7 @@ while not (r < 94 and g < 102 and b < 151):
     hangman = font.render("HANGMAN.", True, (r,g,b))
     screen.blit(hangman, (106,133))
     pygame.display.flip()
-    clock.tick(65)
+    pygame.time.wait(5)
 
 font = pygame.font.Font("bryndan-write-font/BryndanWrite.ttf", 20)
 jeu_du_pendu_educatif = font.render("Jeu du pendu éducatif", True, (94,102,151))
