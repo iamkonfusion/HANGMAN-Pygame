5f7209cc10cb8ddd7b37c863e0c515f38eb65ff8
iamkonfusion <96213674+iamkonfusion@users.noreply.github.com>
2022-05-21 18:19:00 +0200

    Update mecanisme_pendu.py

--- HANGMAN/mecanisme_pendu.py
+++ HANGMAN/mecanisme_pendu.py
@@ -6,7 +6,7 @@ pygame.init()
 pygame.font.init()
 
 size = width, height = 940,540
-screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption('HANGMAN')
 pygame.display.set_icon(pygame.image.load('design/caticon.ico'))
@@ -18,10 +18,11 @@ bgm.play(-1)
 
 well_done = pygame.image.load('design/ingame/well_done.png')
 try_again = pygame.image.load('design/ingame/try_again.png')
+clock = pygame.time.Clock()
 
 screen.blit(pygame.transform.smoothscale(background, size),(0,0))
 
-clock = pygame.time.Clock()
+
 
 """
 font = pygame.font.Font("bryndan-write-font/BryndanWrite.ttf", 97)
@@ -53,7 +54,6 @@ debutant_adj = {"blue":"bleu",
 font = pygame.font.Font("bryndan-write-font/BryndanWrite.ttf", 97)
 color_values = r, g, b = (94,102,151) # ORIGINAL 'HANGMAN.' COLOR VALUES
 color_values = r, g, b = (248,251,255) # couleur background
-clock = pygame.time.Clock()
 
 while not (r < 94 and g < 102 and b < 151):
     if not r < 94 :
@@ -147,3 +147,71 @@ while HANGMAN:
 
 pygame.quit()
 exit()
+
+"""
+
+import pygame
+from sys import exit
+
+pygame.init()
+pygame.font.init()
+
+size = width, height = 940,540
+screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+
+pygame.display.set_caption('HANGMAN')
+pygame.display.set_icon(pygame.image.load('design/caticon.ico'))
+background = pygame.image.load('design/homescreen.png')
+screen.blit(pygame.transform.smoothscale(background, size),(0,0))
+
+font = pygame.font.Font("bryndan-write-font/BryndanWrite.ttf", 97)
+# color_values = r, g, b = (94,102,151) # ORIGINAL 'HANGMAN.' COLOR VALUES
+color_values = r, g, b = (248,251,255) # couleur background
+clock = pygame.time.Clock()
+
+while not (r < 94 and g < 102 and b < 151):
+    if not r < 94 :
+        r -= 1
+    if not g < 102:
+        g -= 1
+    if not b < 151:
+        b -= 1
+    hangman = font.render("HANGMAN.", True, (r,g,b))
+    screen.blit(hangman, (106,133))
+    pygame.display.flip()
+    clock.tick(65)
+
+font = pygame.font.Font("bryndan-write-font/BryndanWrite.ttf", 20)
+jeu_du_pendu_educatif = font.render("Jeu du pendu éducatif", True, (94,102,151))
+tap_here_to_start = font.render("Appuyez sur une touche", True, (210,214,229))
+
+screen.blit(hangman, (106,133))
+screen.blit(jeu_du_pendu_educatif, (339,229))
+screen.blit(tap_here_to_start, (190, 400))
+pygame.display.flip()
+
+Running = True
+while Running :
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            # pygame.display.quit()
+            exit()
+            running = False
+
+        elif event.type == pygame.VIDEORESIZE:
+            screen = pygame.display.set_mode(event.dict['size'], pygame.RESIZABLE)
+            screen.blit(pygame.transform.scale(background, event.dict['size']), (0, 0))
+            screen.blit(hangman, (106,133))
+            screen.blit(jeu_du_pendu_educatif, (339,229))
+            screen.blit(tap_here_to_start, (190, 400))
+            pygame.display.flip()
+
+        if event.type == pygame.KEYDOWN:            # registers 'pressed key' events
+            if event.key == pygame.K_SPACE:
+                print('Spacebar was pressed')
+
+    pygame.display.flip()
+
+"""
