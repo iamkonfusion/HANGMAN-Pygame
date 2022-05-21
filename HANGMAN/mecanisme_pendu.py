de6af5c0c68f74a02313c90b357db3da0f903152
iamkonfusion <96213674+iamkonfusion@users.noreply.github.com>
2022-05-21 19:32:46 +0200

    Update mecanisme_pendu.py

--- HANGMAN/mecanisme_pendu.py
+++ HANGMAN/mecanisme_pendu.py
@@ -29,7 +29,6 @@ screen.blit(hangman, (106,133))
 
 pygame.display.flip()
 
-
 debutant_adj = {"blue":"bleu",
             "difficult":"difficile",
             "other":"autre",
@@ -104,6 +103,12 @@ while HANGMAN:
                 fautes = 0
                 while fautes != 10 and not mot_complet:
 
+                    screen.blit(pygame.transform.smoothscale(background, size),(0,0))
+                    affichage_word_trad = " ".join(display_word) + " = " + " ".join(display_trad)
+                    screen.blit(font_ingame.render(affichage_word_trad, True, (r,g,b)), coord_tirets)
+                    screen.blit(font_ingame.render(str(fautes), 1 , (94, 102, 151)),(850,450)) # ajout !
+                    pygame.display.flip()
+
                     for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                             pygame.quit()
@@ -125,15 +130,18 @@ while HANGMAN:
 
                                 screen.blit(pygame.transform.smoothscale(background, size),(0,0))
                                 affichage_word_trad = " ".join(display_word) + " = " + " ".join(display_trad)
+                                screen.blit(font_ingame.render(str(fautes), 1 , (94, 102, 151)),(850,450)) # ajout !
                                 screen.blit(font_ingame.render(affichage_word_trad, True, (r,g,b)), coord_tirets)
                                 pygame.display.flip()
 
                             else:
                                 fautes += 1
                                 screen.blit(pygame.transform.smoothscale(background, size),(0,0))
-                                screen.blit(font_ingame.render("Mauvaise lettre !", True, (r,g,b)), (350,260))
+                                screen.blit(font_ingame.render(affichage_word_trad, True, (r,g,b)), coord_tirets)
                                 screen.blit(font_ingame.render(str(fautes), 1 , (94, 102, 151)),(850,450)) # ajout !
+                                screen.blit(font_ingame.render("Mauvaise lettre !", True, (r,g,b)), (350,260))
                                 pygame.display.flip()
+                                pygame.time.wait(400)
                                 
                                 if fautes == 10:
                                     screen.blit(pygame.transform.smoothscale(try_again, size),(0,0))
@@ -160,46 +168,6 @@ pygame.quit()
 exit()
 
 """
-
-import pygame
-from sys import exit
-
-pygame.init()
-pygame.font.init()
-
-size = width, height = 940,540
-screen = pygame.display.set_mode(size, pygame.RESIZABLE)
-
-pygame.display.set_caption('HANGMAN')
-pygame.display.set_icon(pygame.image.load('design/caticon.ico'))
-background = pygame.image.load('design/homescreen.png')
-screen.blit(pygame.transform.smoothscale(background, size),(0,0))
-
-font = pygame.font.Font("bryndan-write-font/BryndanWrite.ttf", 97)
-# color_values = r, g, b = (94,102,151) # ORIGINAL 'HANGMAN.' COLOR VALUES
-color_values = r, g, b = (248,251,255) # couleur background
-clock = pygame.time.Clock()
-
-while not (r < 94 and g < 102 and b < 151):
-    if not r < 94 :
-        r -= 1
-    if not g < 102:
-        g -= 1
-    if not b < 151:
-        b -= 1
-    hangman = font.render("HANGMAN.", True, (r,g,b))
-    screen.blit(hangman, (106,133))
-    pygame.display.flip()
-    clock.tick(65)
-
-font = pygame.font.Font("bryndan-write-font/BryndanWrite.ttf", 20)
-jeu_du_pendu_educatif = font.render("Jeu du pendu éducatif", True, (94,102,151))
-tap_here_to_start = font.render("Appuyez sur une touche", True, (210,214,229))
-
-screen.blit(hangman, (106,133))
-screen.blit(jeu_du_pendu_educatif, (339,229))
-screen.blit(tap_here_to_start, (190, 400))
-pygame.display.flip()
 
 Running = True
 while Running :
