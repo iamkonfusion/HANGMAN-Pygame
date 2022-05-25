139e025480b60e10d78a9333cc6b5df351878600
iamkonfusion <96213674+iamkonfusion@users.noreply.github.com>
2022-05-25 21:00:55 +0200

    Update test.py

--- HANGMAN/test.py
+++ HANGMAN/test.py
@@ -2,44 +2,52 @@ import pygame
 from sys import exit
 
 pygame.init()
-pygame.font.init()
 
 size = width, height = 940,540
-screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption('HANGMAN')
 pygame.display.set_icon(pygame.image.load('design/caticon.ico'))
-background = pygame.image.load('design/homescreen.png')
+background = pygame.image.load('design/background.png')
 screen.blit(pygame.transform.smoothscale(background, size),(0,0))
-
-font = pygame.font.Font("bryndan-write-font/BryndanWrite.ttf", 97)
-# color_values = r, g, b = (94,102,151) # ORIGINAL 'HANGMAN.' COLOR VALUES
-color_values = r, g, b = (248,251,255) # couleur background
-clock = pygame.time.Clock()
-
-while not (r < 94 and g < 102 and b < 151):
-    if not r < 94 :
-        r -= 1
-    if not g < 102:
-        g -= 1
-    if not b < 151:
-        b -= 1
-    hangman = font.render("HANGMAN.", True, (r,g,b))
-    screen.blit(hangman, (106,133))
-    pygame.display.flip()
-    clock.tick(65)
-
-font = pygame.font.Font("bryndan-write-font/BryndanWrite.ttf", 20)
-jeu_du_pendu_educatif = font.render("Jeu du pendu éducatif", True, (94,102,151))
-tap_here_to_start = font.render("Appuyez sur une touche", True, (210,214,229))
-
-screen.blit(hangman, (106,133))
-screen.blit(jeu_du_pendu_educatif, (339,229))
-screen.blit(tap_here_to_start, (190, 400))
 pygame.display.flip()
+
+
+################ CLASS BUTTON ####################
+
+
+class Button():
+    def __init__(self, x, y, image, scale):
+        width, height = image.get_width(), image.get_height()
+        self.image = pygame.transform.scale(image,(int(width*scale),int(height*scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.clicked = False
+
+    def draw(self):
+        action = False
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+        if pygame.mouse.get_pressed()[0] == 0 :
+            self.clicked = False
+        # draw button on screen
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+        return action
+
+#LOAD BUTTON IMAGES
+classique_img = pygame.image.load('design/buttons/classique.png').convert_alpha()
+classique_button = Button(100,200, classique_img, 0.15)
+
+##################################################
 
 Running = True
 while Running :
+
+    if classique_button.draw():
+        print ('Mode Partie Classique')
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -47,14 +55,6 @@ while Running :
             # pygame.display.quit()
             exit()
             running = False
-
-        elif event.type == pygame.VIDEORESIZE:
-            screen = pygame.display.set_mode(event.dict['size'], pygame.RESIZABLE)
-            screen.blit(pygame.transform.scale(background, event.dict['size']), (0, 0))
-            screen.blit(hangman, (106,133))
-            screen.blit(jeu_du_pendu_educatif, (339,229))
-            screen.blit(tap_here_to_start, (190, 400))
-            pygame.display.flip()
 
         if event.type == pygame.KEYDOWN:            # registers 'pressed key' events
             if event.key == pygame.K_SPACE:
